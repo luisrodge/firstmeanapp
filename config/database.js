9fb92b0350e88d79797b76695ad40b264a025bb8
Luis Rodriguez <luisrodge@outlook.com>
2018-02-15 19:23:29 -0600

    Update config database url

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://<luis>:<rodge24>@ds139138.mlab.com:39138/firstmeanapp',
+    database: 'mongodb://luis:rodge24@ds139138.mlab.com:39138/firstmeanapp',
     secret: 'mysuperscret'
 };
